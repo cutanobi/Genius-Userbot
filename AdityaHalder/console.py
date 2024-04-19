617820b03a397e8f244577220da382348841aea1
cutanobi <167220489+cutanobi@users.noreply.github.com>
2024-04-19 23:34:09 +0530

    Update console.py

--- AdityaHalder/console.py
+++ AdityaHalder/console.py
@@ -52,7 +52,7 @@ PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
 
 
 # USERBOT DEFAULT IMAGE
-USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://graph.org/file/1217cb1e402b99fa47fdf.jpg")
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/8d977e35d833e7a1896df.jpg")
 
 
 
